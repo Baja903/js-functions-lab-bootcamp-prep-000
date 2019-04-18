57db9c203ce0db32f668feb24a5a069986003d3b
rational-dispatch-0384 <baja903@gmail.com>
2019-04-18 21:30:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
 // write your code below!
-let name=’you’;
-let holiday=’Independence Day’;
-let number=20;
-let mother=”Mother’s Day”;
 
 function happyHolidays(){
-return “Happy holidays!”;
+  return "Happy Holidays";
 }
-happyHolidays();
-
-function happyHolidaysTo(name){
-return `Happy holidays, ${name}!`;
-}
-happyHolidaysTo();
-
-function happyHolidayTo(holiday, name){
-return `Happy ${holiday}, ${name}!`;
-}
-happyHolidayTo();
-
-function holidayCountdown(mother, number){
-return `It’s ${number} days until ${mother}!`;
-}
-holidayCountdown();
